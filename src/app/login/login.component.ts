5c5b88965891c875c012da5ccbfed4d92536a8ff
João Victor Barbosa da Silva <joaobs98@hotmail.com>
2023-10-04 20:13:25 -0300

    adicionado formularios responsivos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   senha: string = '';
+  senha1: string = '';
+  email1: string = '';
   confirmarSenha: string = '';
+  cpf: string = '';
+  telefone: string = '';
+  nome: string = '';
+  email: string = ''; 
+  cadastroSucesso: boolean = false;
+  loginSucesso: boolean = false;
+  senhaNaoCoincide: boolean = false;
+  nomePreenchido: boolean = false;
+  emailPreenchido: boolean = false;
+  senhaPreenchida: boolean = false;
+  confirmarSenhaPreenchida: boolean = false;
+  cpfPreenchido: boolean = false;
+  telefonePreenchido: boolean = false;
+  email1Preenchido: boolean = false;
+  senha1Preenchida: boolean = false;
 
   constructor() { }
 
@@ -15,4 +32,54 @@ export class LoginComponent {
   }
   cadastroVisivel = false;
 
+  fazerLogin() {
+    if (this.email1Preenchido && this.senha1Preenchida) {
+      this.loginSucesso = true;
+    }
+  }
+
+  criarConta() {
+    if (this.senha === this.confirmarSenha) {
+      this.cadastroSucesso = true;
+      this.senhaNaoCoincide = false;
+    } else {
+      this.senhaNaoCoincide = true;
+    }
+  }
+
+  verificarNomePreenchido() {
+    this.nomePreenchido = this.senha.trim() !== '';
+  }
+
+  verificarEmailPreenchido() {
+    this.emailPreenchido = this.email.trim() !== '';
+  }
+
+  verificarSenhaPreenchida() {
+    this.senhaPreenchida = this.senha.trim() !== '';
+  }
+
+  verificarConfirmarSenhaPreenchida() {
+    this.confirmarSenhaPreenchida = this.confirmarSenha.trim() !== '';
+  }
+
+  verificarCpfPreenchido() {
+    this.cpfPreenchido = this.cpf.trim() !== '';
+  }
+
+  verificarTelefonePreenchido() {
+    this.telefonePreenchido = this.telefone.trim() !== '';
+  }
+
+  verificarEmail1Preenchido() {
+    this.email1Preenchido = this.email1.trim() !== '';
+  }
+
+  verificarSenha1Preenchida() {
+    this.senha1Preenchida = this.senha1.trim() !== '';
+  }
 }
+
+
+
+
--- src/app/reenvio-senha/reenvio-senha.component.ts
+++ src/app/reenvio-senha/reenvio-senha.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./reenvio-senha.component.css']
 })
 export class ReenvioSenhaComponent {
+  email: string = '';
+  mensagem: string = '';
 
+  enviarEmail() {
+    if (this.validarEmail(this.email)) {
+      this.mensagem = 'Foi enviado um email com instruções de recuperação.';
+    } else {
+      this.mensagem = 'Email inválido.';
+    }
+  }
+
+  private validarEmail(email: string): boolean {
+    if (!email) {
+      return false;
+    }
+
+    const formatoEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  
+    return formatoEmail.test(email);
+  }
+  
 }
